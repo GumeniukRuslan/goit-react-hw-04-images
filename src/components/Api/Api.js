d8ca07e5427c09fbd3035cfb1cea246c58b7b2db
GumeniukRuslan <bishob1997@gmail.com>
2023-06-09 20:01:28 +0300

    made hooks

--- src/components/Api/Api.js
+++ src/components/Api/Api.js
@@ -15,3 +15,35 @@ export async function apiFetch(searchQuery, page) {
   );
   return response;
 }
+
+export async function firstFetchData(
+  searchPromt,
+  page,
+  setStatus,
+  setImagesArr,
+  setTotalImages
+) {
+  if (!searchPromt) {
+    return;
+  }
+  try {
+    setStatus('pending');
+    const images = await apiFetch(searchPromt, page);
+    setImagesArr(images.data.hits);
+    setTotalImages(images.data.totalHits);
+    setStatus('resolved');
+  } catch (e) {
+    setStatus('rejected');
+  }
+}
+export async function moreFetchData(searchPromt, page, setImagesArr) {
+  if (!searchPromt) {
+    return;
+  }
+  try {
+    const images = await apiFetch(searchPromt, page);
+    setImagesArr(prevArr => [...prevArr, ...images.data.hits]);
+  } catch (e) {
+    alert('Something went wrong');
+  }
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,65 +1,74 @@
-import { Component } from "react";
+import { useEffect, useState } from "react"
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { apiFetch } from "./Api/Api";
 import { Button } from "./Button/Button";
 import { Loader } from "./Loader/Loader";
 import { Message } from "./Message/Message";
+import { toast, Toaster } from "react-hot-toast";
 
-export class App extends Component  {
+export const App = () => {
+  const [searchPromt, setSearchPromt] = useState('');
+  const [page, setPage] = useState(1);
+  const [imagesArr, setImagesArr] = useState([]);
+  const [totalImages, setTotalImages] = useState(0);
+  const [status, setStatus] = useState('idle');
 
-  state = {
-    searchPromt: '',
-    page: 1,
-    imagesArr: [],
-    totalImages: 0,
-    status: 'idle'
-  }
-
- 
-
-  submitHandler = (evt) => {
-    evt.preventDefault()
-    this.setState({ searchPromt: evt.target.elements.search.value.trim(), page: 1 }, async () => {
-      if (!this.state.searchPromt) {
+  useEffect(() => {
+  const fetchData = async () => {
+    if (!searchPromt) {
       return
-      }
-      try {
-        this.setState({status: 'pending'})
-        const images = await apiFetch(this.state.searchPromt, this.state.page)
-        this.setState({ imagesArr: images.data.hits, totalImages: images.data.totalHits, status: 'resolved' })
-      } catch (e) {
-        this.setState({status: 'rejected'})
-      }
-    })
-    
+    }
+    try {
+      setStatus('pending');
+      const images = await apiFetch(searchPromt, page);
+      setImagesArr(images.data.hits);
+      setTotalImages(images.data.totalHits);
+      setStatus('resolved');
+    } catch (e) {
+      setStatus('rejected');
+    }
+  };
+
+  fetchData();
+  }, [searchPromt]);
+  
+  useEffect(() => {
+  const fetchData = async () => {
+    if (!searchPromt) {
+      const notify = () => toast.error('Please, fill the search field.');
+      return notify()
+    }
+    try {
+      const images = await apiFetch(searchPromt, page);
+      setImagesArr(prevArr => [...prevArr, ...images.data.hits]);
+    } catch (e) {
+      setStatus('rejected');
+    }
+  };
+
+  fetchData();
+}, [page]);
+
+  const submitHandler = async (evt) => {
+    evt.preventDefault()
+    if (!evt.target.elements.search.value.trim()) {
+      const notify = () => toast.error('Please, fill the search field.');
+      return notify()
+    }
+    setSearchPromt(evt.target.elements.search.value.trim());
+    setPage(1);
   }
 
-  loadMoreImg =  () => {
-    this.setState({ page: this.state.page + 1 }, async () => {
-       try {
-      const images = await apiFetch(this.state.searchPromt, this.state.page)
-      this.setState(prevState => ({
-        imagesArr: [...prevState.imagesArr, ...images.data.hits]
-        }));
-      } catch (e) {
-        alert('Something went wrong')
-      }
-    })
-   
-  }
-
-  render() {
-     const {status, imagesArr, totalImages} = this.state
-    return (
-      <>
-        <Searchbar onSubmit={this.submitHandler} />
-        <div className="container">
-          {status === 'idle' ? <Message text='Fulfill your imagination'/>
-            : status === 'pending' ? <Loader />
-            : status === 'resolved' ? imagesArr.length === 0 ? <Message text='Nobody here but us chickens!'/> : <><ImageGallery data={imagesArr} />{totalImages > imagesArr.length ? <Button loadMore={this.loadMoreImg} />: <Message text='Hope it`s fulfilled'/>}</>
-            : <Message text='Something went wrong'/>}
-        </div>
-      </>);
-  } 
+  return (
+    <>
+      <Searchbar onSubmit={submitHandler} />
+      <div className="container">
+        {status === 'idle' ? <Message text='Fulfill your imagination'/>
+          : status === 'pending' ? <Loader />
+          : status === 'resolved' ? imagesArr.length === 0 ? <Message text='Nobody here but us chickens!'/> : <><ImageGallery data={imagesArr} />{totalImages > imagesArr.length ? <Button loadMore={()=> setPage(prevPage => prevPage + 1)} />: <Message text='Hope it`s fulfilled'/>}</>
+          : <Message text='Something went wrong' />}
+        <Toaster position="top-right" toastOptions={{duration: 1500}} />
+      </div>
+    </>);
 };
