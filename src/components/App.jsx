b9e7e036f1f16a86355f48de31c6d6b931ced19d
GumeniukRuslan <bishob1997@gmail.com>
2023-06-11 11:02:16 +0300

    upd useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Searchbar } from "./Searchbar/Searchbar";
 import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { apiFetch } from "./Api/Api";
@@ -8,53 +8,51 @@ import { Message } from "./Message/Message";
 import { toast, Toaster } from "react-hot-toast";
 
 export const App = () => {
-  const [searchPromt, setSearchPromt] = useState('');
+  const [searchRequest, setSearchRequest] = useState('');
   const [page, setPage] = useState(1);
   const [imagesArr, setImagesArr] = useState([]);
   const [totalImages, setTotalImages] = useState(0);
   const [status, setStatus] = useState('idle');
 
-  
+  const prevsearchRequestRef = useRef(searchRequest);
   useEffect(() => {
-    if (!searchPromt) {
-      return
-    }
-    (async () => {
+    if (!searchRequest) return;
+    
+    (async () => {  
+      if (prevsearchRequestRef.current !== searchRequest) {
         try {
           setStatus('pending');
-          const images = await apiFetch(searchPromt, page);
+          const images = await apiFetch(searchRequest, 1);
           setImagesArr(images.data.hits);
           setTotalImages(images.data.totalHits);
           setStatus('resolved');
+          prevsearchRequestRef.current = searchRequest;
         } catch (e) {
           setStatus('rejected');
         }
-      })();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchPromt]);
-    
-  useEffect(() => {
-    if (page === 1) {
-      return;
-    }
-    (async () => {
+      }
+
+      if (page > 1) {
         try {
-          const images = await apiFetch(searchPromt, page);
+          const images = await apiFetch(searchRequest, page);
           setImagesArr(prevArr => [...prevArr, ...images.data.hits]);
         } catch (e) {
           setStatus('rejected');
         }
-      })();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+      }
+    })();
+  }, [searchRequest, page]);
 
-  const submitHandler = async (evt) => {
+  const submitHandler = evt => {
     evt.preventDefault()
     if (!evt.target.elements.search.value.trim()) {
       const notify = () => toast.error('Please, fill the search field.');
       return notify()
+    } else if (evt.target.elements.search.value.trim() === searchRequest) {
+      const notify = () => toast('Please use another request ;)', {icon: '😵‍💫'});;
+      return notify()
     }
-    setSearchPromt(evt.target.elements.search.value.trim());
+    setSearchRequest(evt.target.elements.search.value.trim());
     setPage(1);
   }
 
@@ -68,7 +66,7 @@ export const App = () => {
           : <><ImageGallery data={imagesArr} />{totalImages > imagesArr.length ? <Button loadMore={() => setPage(prevPage => prevPage + 1)} />
             : <Message text='Hope it`s fulfilled' />}</> : <></>}
         {status === 'rejected' && <Message text='Something went wrong' />}
-        <Toaster position="top-right" toastOptions={{duration: 1500}} />
+        <Toaster position="top-right" toastOptions={{duration: 1500, style: {border: '1px solid #713200'}}} />
       </div>
     </>);
 };
